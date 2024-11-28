ad7db70cd18eb367fcc541e60d43fbc4e4f7f281
YAANNN. <Rakhyan40@gmail.com>
2024-11-28 21:30:37 +0700

    one

--- App.js
+++ App.js
@@ -4,10 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      {/* Teks di tengah layar */}
+
       <Text style={styles.text}>Teknik Informatika</Text>
 
-      {/* Dua kotak dengan properti flexbox */}
       <View style={styles.boxContainer}>
         <View style={[styles.box, styles.box1]} />
         <View style={[styles.box, styles.box2]} />
@@ -19,30 +18,30 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D3D3D3', // Latar belakang abu-abu terang
-    justifyContent: 'center', // Memusatkan isi secara vertikal
-    alignItems: 'center', // Memusatkan isi secara horizontal
+    backgroundColor: '#D3D3D3', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
   },
   text: {
     fontSize: 24,
-    color: 'blue', // Warna teks biru
-    fontWeight: 'bold', // Gaya teks tebal
-    marginBottom: 20, // Jarak antara teks dan kotak
+    color: 'blue', 
+    fontWeight: 'bold', 
+    marginBottom: 20, 
   },
   boxContainer: {
-    flexDirection: 'row', // Membuat tata letak horizontal
-    justifyContent: 'space-between', // Jarak merata di antara kotak
-    width: '80%', // Mengatur lebar container untuk jarak horizontal
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+    width: '80%', 
   },
   box: {
     width: 100,
     height: 100,
   },
   box1: {
-    backgroundColor: 'red', // Warna kotak 1
+    backgroundColor: 'black', 
   },
   box2: {
-    backgroundColor: 'green', // Warna kotak 2
+    backgroundColor: 'white', 
   },
 });
 
